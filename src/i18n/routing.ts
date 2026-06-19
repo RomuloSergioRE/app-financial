3abc005e0c6fc87063a7aad95356a04162320840
romulo <rsrevan2000zsx@gmail.com>
2026-06-19 10:01:41 -0300

    feat: remove locale prefix from URLs (localePrefix: never)

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,7 +4,7 @@ import { createNavigation } from "next-intl/navigation";
 export const routing = defineRouting({
   locales: ["pt-BR", "en-US", "es-ES"],
   defaultLocale: "pt-BR",
-  localePrefix: "always",
+  localePrefix: "never",
 });
 
 export const { Link, redirect, usePathname, useRouter, getPathname } = createNavigation(routing);
